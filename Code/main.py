7133ae080929db7fd737399a878ea1434feaf58f
mohfarhan20 <ghifarifarhan20@gmail.com>
2023-03-07 00:02:46 +0700

    hide visibility of streamlit footer

--- Code/main.py
+++ Code/main.py
@@ -1,11 +1,25 @@
 import streamlit as st
 
+# CSS 
+st.markdown("""
+<style>
+.css-h5rgaw.egzxvld1
+{
+    visibility:hidden;
+}
+.css-9s5bis.edgvbvh3
+{
+    visibility:hidden;
+}
+</style>
+""",unsafe_allow_html=True)
+
 # set the title
 
 st.title('Pothole Detection using YOLO v7')
 st.subheader(" Deteksi jalan berlubang dengan menggunakan YOLO V7")
 
-st.text(" Silahkan masukkan video yang akan di scan")
+
 
 
 # Create file uploader
@@ -18,10 +32,15 @@ if video_file is not None:
 
     # Display video player
     st.video(video_bytes)
+else :
+    st.text(" Silahkan masukkan video yang akan di scan")
 
-# Save file
+
+Save file
 with open("vid_1.mp4", "wb") as f:
     f.write(video_bytes)
     st.success("File saved")
 
 st.subheader("Scanning pothole")
+
+
